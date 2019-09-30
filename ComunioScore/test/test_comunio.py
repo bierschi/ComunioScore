e6e65e5fa6229429fc98f0762e8f7ba6f86630d0
Christian Bierschneider <christian.bierschneider@web.de>
2019-09-30 19:10:56 +0200

    test travis yml

--- ComunioScore/test/test_comunio.py
+++ ComunioScore/test/test_comunio.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import configparser
 from ComunioScore.comunio import Comunio
@@ -8,18 +9,18 @@ class TestComunio(unittest.TestCase):
 
     def setUp(self) -> None:
         # load config file
-        self.config = configparser.ConfigParser()
-        self.config.read(ROOT_DIR + '/config/cfg.ini')
+        #self.config = configparser.ConfigParser()
+        #self.config.read(ROOT_DIR + '/config/cfg.ini')
 
         # comunio config
-        self.username = self.config.get('comunio', 'username')
-        self.password = self.config.get('comunio', 'password')
+        #self.username = self.config.get('comunio', 'username')
+        #self.password = self.config.get('comunio', 'password')
 
         self.comunio = Comunio()
 
     def test_login(self):
 
-        login = self.comunio.login(username=self.username, password=self.password)
+        login = self.comunio.login(username=os.environ['comunio_username'], password=os.environ['comunio_password'])
         self.assertTrue(login, msg="Login process for comunio failed")
 
     def tearDown(self) -> None:
