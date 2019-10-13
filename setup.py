630ca67e92230a8c1a8fd0d73fc455ac089cfdc0
Christian Bierschneider <christian.bierschneider@web.de>
2019-10-13 14:29:58 +0200

    Tested install command in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+import os
+import subprocess
 from setuptools import setup, find_packages
-
+from setuptools.command.install import install
 from ComunioScore import __version__, __author__, __email__, __license__
 
 with open('requirements.txt') as f:
@@ -11,6 +13,17 @@ with open("README.md", encoding='utf-8') as f:
 with open("CHANGELOG.rst") as f:
     changelog = f.read()
 
+
+class CustomInstallCommand(install):
+
+    def run(self):
+        install.run(self)
+        current_dir_path = os.path.dirname(os.path.realpath(__file__))
+        service_path = os.path.join(current_dir_path, 'service', 'ComunioScoreService.sh')
+        print(service_path)
+        subprocess.check_output([service_path])
+
+
 setup(
     name="ComunioScore",
     version=__version__,
@@ -21,9 +34,10 @@ setup(
     author_email=__email__,
     url="https://github.com/bierschi/ComunioScore",
     packages=find_packages(),
-    data_files=[
-        ('/etc/systemd/system', ['service/ComunioScoreApp.service'])
-    ],
+    include_package_data=True,
+    #data_files=[
+    #    ('/etc/systemd/system', ['service/ComunioScoreApp.service'])
+    #],
     install_requires=required,
     keywords=["Comunio", "Sofascore", "Rating", "Score"],
     classifiers=[
@@ -46,4 +60,7 @@ setup(
         ],
     },
     zip_safe=False,
+    #cmdclass={
+    #    'install': CustomInstallCommand
+    #},
 )
