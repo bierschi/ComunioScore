92bb5ae3ba39ee68fc0ac808345f6fca4cff276d
Christian Bierschneider <christian.bierschneider@web.de>
2019-10-25 12:06:50 +0200

    Added new methods to ComunioScoreTelegram

--- ComunioScore/messenger/comunioscore_telegram.py
+++ ComunioScore/messenger/comunioscore_telegram.py
@@ -1,6 +1,7 @@
 import logging
 import configparser
 from telegram.ext import Updater, CommandHandler
+from telegram.bot import Bot
 from ComunioScore import ROOT_DIR
 
 config = configparser.ConfigParser()
@@ -12,11 +13,14 @@ class ComunioScoreTelegram:
 
     def __init__(self, token):
         self.logger = logging.getLogger('ComunioScoreApp')
-        self.logger.info('create class Telegram')
+        self.logger.info('create class ComunioScoreTelegram')
 
         self.token = token
         self.updater = Updater(token=self.token)
         self.dp = self.updater.dispatcher
+
+        self.bot = Bot(token=self.token)
+        self.comunioscore_chatid = -394160563
 
     def __del__(self):
         pass
@@ -24,7 +28,6 @@ class ComunioScoreTelegram:
     def run(self):
         """ runs the telegram updater
 
-        :return:
         """
         self.updater.start_polling()
         self.updater.idle()
@@ -32,8 +35,8 @@ class ComunioScoreTelegram:
     def add_handler(self, command, handler):
         """ add a handler function to the dispatcher
 
-        :param command:
-        :param handler:
+        :param command: command msg
+        :param handler: handler function to execute
         """
         self.dp.add_handler(handler=CommandHandler(command=command, callback=handler))
 
@@ -43,9 +46,26 @@ class ComunioScoreTelegram:
         :return:
         """
         chat_id = update.message.chat_id
+        print(chat_id)
         bot.send_message(chat_id=chat_id, text="hallo")
 
+    def new_msg(self, text):
+        """ new text message for the bot
+
+        """
+        self.logger.info("send new message")
+        self.bot.sendMessage(chat_id=self.comunioscore_chatid, text=text)
+
+    def new_photo(self):
+        """
+
+        :return:
+        """
+        self.bot.sendPhoto(chat_id=self.comunioscore_chatid)
+        pass
 
 if __name__ == '__main__':
     tele = ComunioScoreTelegram(token=token)
+    #tele.add_handler('start', tele.send_msg)
+    tele.new_msg(text="was geht")
     tele.run()
